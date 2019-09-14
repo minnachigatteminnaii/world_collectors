9ad2fe35e85bfda47ef09259a61f8c732044f5f4
ellie-fountain <tenoclockeffct@gmail.com>
2019-09-14 10:07:48 +0000

    create myp

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   def show
+  	@user = current_user
   end
 
   def edit
@@ -7,4 +8,16 @@ class UsersController < ApplicationController
 
   def remove
   end
+
+  def update
+  	user = user.find(current_user.id)
+  	user is_delete = remove
+  	user.save
+  	redirect_to shopping_addresses_path
+  end
+private
+def user_params
+    params.require(:user).permit(:first_name, :last_name, :kana_first, :kana_last, :phone_numeber, :address, :postal_code, :email, :profile_image)
+end
+
 end
