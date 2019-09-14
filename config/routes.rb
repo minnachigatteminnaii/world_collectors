5b7121fed86137a241c6b9b9ccdaa5a00b90891e
s022a8 <kafuka821shiro@gmail.com>
2019-09-14 05:57:01 +0000

    Add routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,90 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  ###ルートURL
+  root 'items#index'
+
+
+  ###エンドユーザー###
+  #UsersController
+  resources :users, only: [:show, :edit, :update] do
+    collection do
+      get 'order'
+      get 'warn'
+      put 'unsubscribe'
+    end
+  end
+  
+  #ItemsController
+  resources :items, only: :show
+  get '/items/category/:category_id', to: 'items#category'
+
+  #CartsController
+  resources :carts, only: [:index, :create, :destroy]
+  put '/carts/:id', to: 'carts#change'
+
+  #Shopping_AddressesController
+  resources :shopping_addresses, only: [:index, :new, :create, :edit, :update, :destroy]
+
+  #OrdersController
+  resources :orders, only: [:new, :create] do
+    collection do
+      get 'confirm_get' ##設計書と違う点
+      post 'confirm_post' ##設計書と違う点
+      get 'done'
+    end
+  end
+
+  #FavoritesController
+  resources :favorites, only: [:index, :create, :destroy]
+
+  #ContactsController
+  resources :contacts, only: [:new, :create]
+
+
+
+  ###管理者###
+  #Admin::UsersController
+  get '/admin', to: 'admin/users#top' #管理者側のルートURL
+
+  #Admin::End_UsersController
+  namespace :admin do
+    resources :end_users, only: [:index, :show, :edit, :update]
+    put '/end_users/remove/:id', to: 'end_users#remove', as: :remove_path ##設計と違う箇所
+  end
+
+  #Admin::ItemsController
+  namespace :admin do
+    resources :items, only: [:index, :show, :edit, :update, :new, :create]
+    put 'items/stop/:id', to: 'items#stop', as: :stop_path
+  end
+
+  #Admin::OrdersControler
+  namespace :admin do
+    resources :orders, only: [:index, :show]
+  end
+
+  #Admin::ArrivalsController
+  namespace :admin do
+    resources :arrivals, only: [:index, :new, :create] do
+      collection do
+        get 'result'
+      end
+    end
+  end
+
+  #Admin::ContactsController
+  namespace :admin do
+    resources :contacts, only: [:index, :show]
+  end
+
+
+
+  ###Devise
+  #Users::~~DeviseController
+
+
+  #Administrators::~~DeviseController
+  
 end
+
