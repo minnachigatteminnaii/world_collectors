2ec1aea2eb3a7667f71ab79c9616b4de74b6a556
s022a8 <kafuka821shiro@gmail.com>
2019-09-14 14:39:44 +0000

    Add items show and carts create

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,2 +1,27 @@
 class CartsController < ApplicationController
+    def index
+    end
+
+    def create
+        @cart_item = current_user.cart_items.build(cart_item_params)
+        @cart_item.update_attributes(item_id: params[:item])
+        if @cart_item.save
+            redirect_to root_url
+        else
+            redirect_to item_path(params[:item])
+        end
+    end
+
+    def destroy
+    end
+
+    def change
+    end
+
+
+    private
+
+        def cart_item_params
+            params.require(:cart_item).permit(:quantity)
+        end
 end
