cf3d41f79b07a8aebccd73c80713014a4272de64
s022a8 <kafuka821shiro@gmail.com>
2019-09-14 16:28:06 +0000

    Add carts index destroy change

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,5 +1,6 @@
 class CartsController < ApplicationController
     def index
+        @user_items = current_user.items   #Userに紐づくItemを全て持ってくる
     end
 
     def create
@@ -8,14 +9,21 @@ class CartsController < ApplicationController
         if @cart_item.save
             redirect_to root_url
         else
-            redirect_to item_path(params[:item])
+            redirect_to item_url(params[:item])
         end
     end
 
     def destroy
+        #@cart_item = current_user.cart_items.find_by(item_id: params[:id])
+        @cart_item = CartItem.find_by(user_id: current_user.id, item_id: params[:id])
+        @cart_item.destroy
+        redirect_to carts_url
     end
 
     def change
+        @cart_item = CartItem.find_by(user_id: current_user.id, item_id: params[:id])
+        @cart_item.update_attributes(cart_item_params)
+        redirect_to carts_url
     end
 
 
