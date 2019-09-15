ee968785f93a9ea115344b0946c18f46646a3fbf
s022a8 <kafuka821shiro@gmail.com>
2019-09-15 03:58:01 +0000

    Add Relation and Validate and Enum

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,2 +1,6 @@
 class Artist < ApplicationRecord
+    has_many :items, through: :artists_items
+    has_many :artists_items
+
+    validates :name, presence: true
 end
--- app/models/orders_item.rb
+++ app/models/orders_item.rb
@@ -1,2 +1,8 @@
 class OrdersItem < ApplicationRecord
+    belongs_to :item
+    belongs_to :order
+
+    validates :user_id ,presence: true
+    validates :item_id, presence: true
+    validates :quantity, presence: true
 end
