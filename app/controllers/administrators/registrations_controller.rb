2526fa29350635449cfda7594c91ac10bc8eb5eb
mmm <minamimohumohu@gmail.com>
2019-09-15 11:54:34 +0000

    “サインイン・サインアップ画面を修正しました。”

--- app/controllers/administrators/registrations_controller.rb
+++ app/controllers/administrators/registrations_controller.rb
@@ -51,9 +51,9 @@ class Administrators::RegistrationsController < Devise::RegistrationsController
   # end
 
   # The path used after sign up.
-  # def after_sign_up_path_for(resource)
-  #   super(resource)
-  # end
+   def after_sign_up_path_for(resource)
+    admin_items_path(resource)
+   end
 
   # The path used after sign up for inactive accounts.
   # def after_inactive_sign_up_path_for(resource)
