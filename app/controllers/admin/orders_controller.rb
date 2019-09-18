0e77fd64df3fb3183631d22632161374561a6290
s022a8 <kafuka821shiro@gmail.com>
2019-09-18 08:26:54 +0000

    Finish receive index show

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,2 +1,10 @@
 class Admin::OrdersController < ApplicationController
+
+    def index
+        @receives = Order.all
+    end
+
+    def show
+        @receive = Order.find(params[:id])
+    end
 end
